7cea58ba271a9cc6b9d573a5ceef3daf513a290d
sashankL-01 <lekkalas@iitbhilai.ac.in>
2025-02-15 21:46:36 +0530

    first upload

---
+++ mini_project/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
